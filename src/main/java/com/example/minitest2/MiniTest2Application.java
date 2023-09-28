7d6c0deab73353dc6e97a5cdebd099f1bc16c61d
Tuan Hai <vutuanhai.18101998@gmail.com>
2023-09-28 19:13:00 +0700

    add

---
+++ src/main/java/com/example/minitest2/MiniTest2Application.java
@@ -0,0 +1,13 @@
+package com.example.minitest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniTest2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniTest2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/minitest2/MiniTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minitest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniTest2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
